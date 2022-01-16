d30052fd6bfbbf0702704109e652fe6b649d36ac
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-01-16 19:27:44 +0530

    Update script.js

--- environments/updates/script.js
+++ environments/updates/script.js
@@ -4,13 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("currentVersion").innerText = require(`${__dirname}/../../package.json`).version || "2.0.0-dev";
 
     const state = document.getElementById("status");
-
     ipcRenderer.on("checking-for-update", (e) => {
         state.innerHTML = "Checking for updates from AHQSoftwares DB...";
     });
 
     ipcRenderer.on("new-update", (e, version) => {
-        state.innerHTML = `Update found <b>${version}</b>!`;
+        let type = "patch";
+        if ((__dirname}/../../package.json`).version - version < 1) {
+        type="minor";
+        } else if((__dirname}/../../package.json`).version - version < 0.1) {
+        type= "patch"
+        } else {
+            type= "major"
+        }
+        state.innerHTML = `Update found Type: <b>${type}</b> Tag: <b>${version}</b>!`;
     });
 
     ipcRenderer.on("download-progress", (e, progress) => {
